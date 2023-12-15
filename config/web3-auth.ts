5e121dff07f052e5cfb26e6c153681f017e2df37
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-12-15 10:50:01 -0500

    fix: optional mfa (#30)

--- config/web3-auth.ts
+++ config/web3-auth.ts
@@ -1,10 +1,24 @@
 import { OPENLOGIN_NETWORK_TYPE, WALLET_ADAPTERS } from "@web3auth/base"
+import {
+  MFA_LEVELS,
+  OpenLoginOptions,
+  UX_MODE,
+} from "@toruslabs/openlogin-utils"
+import { LoginSettings } from "@web3auth/openlogin-adapter"
 
 export const WEB3AUTH_CLIENT_ID =
   process.env["NEXT_PUBLIC_WEB3AUTH_CLIENT_ID"] ?? "invalid"
 export const WEB3AUTH_NETWORK = process.env[
   "NEXT_PUBLIC_WEB3AUTH_NETWORK"
 ] as OPENLOGIN_NETWORK_TYPE
+
+export const WEB3AUTH_ADAPTER_SETTINGS = {
+  uxMode: UX_MODE.POPUP,
+} as OpenLoginOptions
+
+export const WEB3AUTH_LOGIN_SETTINGS = {
+  mfaLevel: MFA_LEVELS.OPTIONAL,
+} as LoginSettings
 
 export const WEB3AUTH_MODAL_CONFIG = {
   [WALLET_ADAPTERS.OPENLOGIN]: {
