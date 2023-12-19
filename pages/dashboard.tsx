a9e5e91c3ab4bc7407ef9c97aa9b2f6ebb605ad7
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-12-19 15:30:33 -0500

    feat: handle login when displaying dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,21 @@
 import useWeb3AuthStore from "../store/web3-auth"
 import DashboardComponent from "../components/dashboard"
 import { Box, Container, Grid, GridItem } from "@chakra-ui/react"
+import { useEffect, useMemo } from "react"
+import useAuthHandlers from "../hooks/useAuthHandlers"
 
 export default function Dashboard() {
   const store = useWeb3AuthStore()
   const isConnected = store.isConnected()
+  const { handleLogin } = useAuthHandlers()
+  const memoizedLogin = useMemo(() => handleLogin, [])
+
+  // TODO: Better way to handle this to re-connect to the session
+  useEffect(() => {
+    if (!isConnected) {
+      memoizedLogin()
+    }
+  }, [memoizedLogin])
 
   return isConnected ? (
     <Container maxW="6xl" minH={"80vh"}>
