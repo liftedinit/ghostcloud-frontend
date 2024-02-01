5a1208e94b778ceef921c2a02de3bf21d5ffa498
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-02-01 13:59:55 -0500

    fix: description must have at least 1 char

--- components/info.tsx
+++ components/info.tsx
@@ -4,7 +4,7 @@ import { FaInfoCircle } from "react-icons/fa"
 export const tips = {
   name: "The randomly generated identifier for your deployment.",
   description:
-    "A description for your deployment. This must be between 0-500 characters in length.",
+    "A description for your deployment. This must be between 1-500 characters in length.",
   domain: "The domain where your site will be hosted.",
   memo: "A string associated with a transaction in Web3",
   file: "Select a zip file less that 5MB in size which contains your site build. This archive must contain an index.html file in the root.",
